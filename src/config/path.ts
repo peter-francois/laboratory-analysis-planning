7a7f2906be3102e0a05faa50e9186cb28b0ce29b
Peter Francois <peter.francois237@hotmail.com>
2026-03-03 14:59:28 +0100

    feat: create first output from data

---
+++ src/config/path.ts
@@ -0,0 +1,3 @@
+import * as path from "path";
+
+export const DEFAULT_OUTPUT_FILE = path.join(__dirname, "../../output/output.json");
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import * as fs from "fs";
 import { readFileSync } from "fs";
 import { InputInterface } from "./types/interface/input.interface";
 import { InputSchema } from "./types/schema/input.schema";
+import { planifyLab } from "./services/schedule.service";
+import { DEFAULT_OUTPUT_FILE } from "./config/path";
 
 function main() {
   const filePath: string = process.argv[2];
@@ -16,6 +18,7 @@ function main() {
     const fileContent = readFileSync(filePath, "utf-8");
     const parsedData: InputInterface = JSON.parse(fileContent);
 
+    // Zod validation of inut file
     const result = InputSchema.safeParse(parsedData);
 
     if (!result.success) {
@@ -23,7 +26,13 @@ function main() {
       process.exit(1);
     }
 
-    console.log("Fichier JSON chargé avec sucès :", parsedData);
+    const data: InputInterface = result.data as InputInterface
+
+    const schedule = planifyLab(data)
+
+    // Export in JSON
+    fs.writeFileSync(DEFAULT_OUTPUT_FILE, JSON.stringify(schedule, null, 2))
+
   } catch (error) {
     console.error("Erreur lors du traitement :", error);
   }
