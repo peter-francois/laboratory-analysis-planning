8b81fa299b6fa91c26e90a2206db082f06cd0712
Peter Francois <peter.francois237@hotmail.com>
2026-03-03 15:02:04 +0100

    fix: align output path with input directory structure

--- src/config/path.ts
+++ src/config/path.ts
@@ -1,3 +1,3 @@
 import * as path from "path";
 
-export const DEFAULT_OUTPUT_FILE = path.join(__dirname, "../../output/output.json");
+export const DEFAULT_OUTPUT_FILE = path.join(__dirname, "../../output/data/output.json");
