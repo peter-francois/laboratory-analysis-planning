12a36ed680a434ecc7b500a82f22b0d30ae12cef
Peter Francois <peter.francois237@hotmail.com>
2026-03-04 16:54:25 +0100

    refactror: create findNextAvailableSlot function

--- src/services/planing.service.ts
+++ src/services/planing.service.ts
@@ -4,9 +4,8 @@ import { EquipmentInterface } from "../types/interface/equipment.interface";
 import { SampleInterface } from "../types/interface/sample.interface";
 import { ScheduleInterface } from "../types/interface/schedule.interface";
 import { TechnicianInterface } from "../types/interface/technician.interface";
-import { shiftTimeByMinutes } from "../utils/time.utils";
 import { timeString } from "../types/common.type";
-import { isResourceAvailable } from "./resource.service";
+import { findNextAvailableSlot } from "./resource.service";
 import { isTechnicianCompatible } from "./technician.service";
 import { isEquipmentCompatible } from "./equipment.service";
 
@@ -24,42 +23,27 @@ export function assignSampleToResources(
 
     const techStart = DateTime.fromISO(technician.startTime);
     const techEnd = DateTime.fromISO(technician.endTime);
-    let currentStartTime = DateTime.max(DateTime.fromISO(sampleArrival), techStart).toFormat(
+    const earliestStart = DateTime.max(DateTime.fromISO(sampleArrival), techStart).toFormat(
       "HH:mm",
     ) as timeString;
-    let currentEndTime = shiftTimeByMinutes(currentStartTime, sampleDuration);
-    if (DateTime.fromISO(currentEndTime) > techEnd) {
-      continue;
-    }
 
     for (const equip of equipment) {
       if (!isEquipmentCompatible(equip, sample.type)) continue;
+      const slot = findNextAvailableSlot(
+        schedule,
+        technician,
+        equip,
+        earliestStart,
+        sampleDuration,
+        techEnd,
+      );
 
-      while (
-        (!isResourceAvailable(
-          schedule,
-          technician,
-          currentStartTime,
-          currentEndTime,
-          "technician",
-        ) ||
-          !isResourceAvailable(schedule, equip, currentStartTime, currentEndTime, "equipment")) &&
-        DateTime.fromISO(currentEndTime) <= techEnd
-      ) {
-        currentStartTime = shiftTimeByMinutes(currentStartTime, 1);
-        currentEndTime = shiftTimeByMinutes(currentStartTime, sampleDuration);
-      }
-
-      if (
-        isResourceAvailable(schedule, technician, currentStartTime, currentEndTime, "technician") &&
-        isResourceAvailable(schedule, equip, currentStartTime, currentEndTime, "equipment") &&
-        DateTime.fromISO(currentEndTime) <= techEnd
-      ) {
+      if (slot) {
         return {
           technicianId: technician.id,
           equipmentId: equip.id,
-          startTime: currentStartTime,
-          endTime: currentEndTime,
+          startTime: slot.startTime,
+          endTime: slot.endTime,
         };
       }
     }
