79aa37d81b6f7627f44c139dd3576f8f5d3f4866
Peter Francois <peter.francois237@hotmail.com>
2026-03-04 08:46:51 +0100

    feat: sort samples by arrivalTime after priority

--- src/services/sample.service.ts
+++ src/services/sample.service.ts
@@ -1,3 +1,4 @@
+import { DateTime } from "luxon";
 import { SamplePriorityEnum } from "../types/enum/sample.enum";
 import { SampleInterface } from "../types/interface/sample.interface";
 
@@ -8,5 +9,17 @@ const priorityRank: Record<SamplePriorityEnum, number> = {
 };
 
 export function orderSamples(samples: SampleInterface[]): SampleInterface[] {
-  return samples.sort((a, b) => priorityRank[a.priority] - priorityRank[b.priority]);
+  return samples.sort((a, b) => {
+    // Sort by priority
+    const priorityDiff = priorityRank[a.priority] - priorityRank[b.priority];
+    if (priorityDiff !== 0) {
+      return priorityDiff;
+    }
+
+    // if same priority, sort by arrivalTime
+    const timeA = DateTime.fromFormat(a.arrivalTime, "HH:mm");
+    const timeB = DateTime.fromFormat(b.arrivalTime, "HH:mm");
+
+    return timeA.toMillis() - timeB.toMillis();
+  });
 }
